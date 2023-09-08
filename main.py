15d9dafef6b072615c8afee943b58f955c87e44b
Harsimran Sidhu <22066581+PKHarsimran@users.noreply.github.com>
2023-09-08 10:40:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import time
 import requests
 import hashlib
 import logging
+import os
 
 # Configuration
 URL = "https://www.google.com/"
 FILENAME = "hash.txt"
-SLEEP_INTERVAL_SECONDS = 3600  # 1 hour
 
 # Configure logging
 logging.basicConfig(filename="website_monitor.log", level=logging.INFO, format="%(asctime)s - %(levelname)s: %(message)s")
 
 def get_hash(url):
     try:
-        response = requests.get(url)
+        headers = {"User-Agent": "Mozilla/5.0"}
+        response = requests.get(url, headers=headers, timeout=10)
         response.raise_for_status()  # Raise an exception for HTTP errors
         return hashlib.sha256(response.content).hexdigest()
     except requests.exceptions.RequestException as e:
@@ -27,23 +27,31 @@ def save_hash(hash_value, filename):
     except Exception as e:
         logging.error(f"Error while saving hash to {filename}: {e}")
 
+def read_initial_hash(filename):
+    try:
+        with open(filename, "r") as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        return None
+
 def check_website_for_changes():
-    initial_hash = get_hash(URL)
-    if initial_hash is None:
+    initial_hash = read_initial_hash(FILENAME)
+
+    current_hash = get_hash(URL)
+    if current_hash is None:
+        logging.warning("Current hash could not be generated. Exiting.")
         return
 
-    while True:
-        current_hash = get_hash(URL)
+    if initial_hash is None:
+        logging.info("Initial hash not found, saving current hash.")
+        save_hash(current_hash, FILENAME)
+        return
 
-        if current_hash is None:
-            return
-
-        if current_hash != initial_hash:
-            logging.info("The website has changed!")
-            save_hash(current_hash, FILENAME)
-            initial_hash = current_hash
-
-        time.sleep(SLEEP_INTERVAL_SECONDS)
+    if current_hash != initial_hash:
+        logging.info("The website has changed!")
+        save_hash(current_hash, FILENAME)
+    else:
+        logging.info("No change detected.")
 
 def main():
     logging.info("Website monitoring started.")
