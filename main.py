663ef091606634914d40b8946e5511b66c4dbb88
Harsimran Sidhu <22066581+PKHarsimran@users.noreply.github.com>
2023-11-19 21:05:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,78 +1,58 @@
 import requests
 import hashlib
 import logging
-import os
 
-# Configuration section: URL to monitor and filename to save hash values
+# Set the URL to monitor and file for storing the hash
 URL = "https://docs-cortex.paloaltonetworks.com/r/Cortex-XDR/Cortex-XDR-Agent-Releases/Cortex-XDR-Agent-Releases"
-FILENAME = "hash.txt"
+HASH_FILE = "hash.txt"
 
-# Configure logging to write logs to a file with a certain format
-logging.basicConfig(filename="website_monitor.log", level=logging.INFO, format="%(asctime)s - %(levelname)s: %(message)s")
+# Configure logging
+logging.basicConfig(filename="monitor.log", level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 
-# Function to get the hash of a website's content
-def get_hash(url):
+def fetch_content(url):
+    """Fetch the webpage content from the URL."""
     try:
-        # Set a User-Agent header to act like a web browser
-        headers = {"User-Agent": "Mozilla/5.0"}
-        # Make an HTTP GET request and set a timeout
-        response = requests.get(url, headers=headers, timeout=10)
-        # Raise an exception if an HTTP error occurs
+        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=10)
         response.raise_for_status()
-        # Calculate and return the SHA-256 hash of the content
-        return hashlib.sha256(response.content).hexdigest()
-    except requests.exceptions.RequestException as e:
-        # Log any request exceptions and return None
-        logging.error(f"Error while fetching {url}: {e}")
+        return response.content
+    except requests.RequestException as error:
+        logging.error(f"Error fetching {url}: {error}")
         return None
 
-# Function to save a hash value to a file
-def save_hash(hash_value, filename):
-    try:
-        with open(filename, "w") as f:
-            f.write(hash_value)
-    except Exception as e:
-        # Log any file-related exceptions
-        logging.error(f"Error while saving hash to {filename}: {e}")
+def compute_hash(content):
+    """Compute the SHA-256 hash of the webpage content."""
+    return hashlib.sha256(content).hexdigest() if content else None
 
-# Function to read the initial hash value from a file
-def read_initial_hash(filename):
+def file_operations(file_name, mode, content=None):
+    """Read from or write to a file."""
     try:
-        with open(filename, "r") as f:
-            return f.read().strip()
+        with open(file_name, mode) as file:
+            if mode == "r":
+                return file.read().strip()
+            elif mode == "w":
+                file.write(content)
     except FileNotFoundError:
-        # If the file doesn't exist, return None
-        return None
+        return None if mode == "r" else logging.error("File not found for writing.")
+    except Exception as error:
+        logging.error(f"File operation error: {error}")
 
-# Function to check a website for changes based on its content hash
-def check_website_for_changes():
-    # Read the initial hash value from the file
-    initial_hash = read_initial_hash(FILENAME)
+def monitor_website():
+    """Check for changes in the website content."""
+    original_hash = file_operations(HASH_FILE, "r")
+    content = fetch_content(URL)
+    current_hash = compute_hash(content)
 
-    # Fetch the current hash value of the website
-    current_hash = get_hash(URL)
-    if current_hash is None:
-        logging.warning("Current hash could not be generated. Exiting.")
+    if not current_hash:
+        logging.warning("Unable to generate current hash.")
         return
 
-    # If no initial hash was found, save the current hash
-    if initial_hash is None:
-        logging.info("Initial hash not found, saving current hash.")
-        save_hash(current_hash, FILENAME)
-        return
-
-    # Compare the current hash with the initial hash and log any changes
-    if current_hash != initial_hash:
-        logging.info("The website has changed!")
-        save_hash(current_hash, FILENAME)
+    if not original_hash or current_hash != original_hash:
+        message = "Website changed!" if original_hash else "Saving initial hash."
+        logging.info(message)
+        file_operations(HASH_FILE, "w", current_hash)
     else:
-        logging.info("No change detected.")
+        logging.info("No changes detected.")
 
-# Main function to execute the script
-def main():
-    logging.info("Website monitoring started.")
-    check_website_for_changes()
-
-# If the script is run directly, execute the main function
 if __name__ == "__main__":
-    main()
+    logging.info("Starting website monitoring.")
+    monitor_website()
